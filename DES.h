23e748ba8e86824c01f4bf827f08e902230daf22
amper43 <amper43@ya.ru>
2014-02-01 01:36:18 -0800

    Delete DES.h

--- DES.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DES_H
-#define _DES_H
-
-typedef char bool;
-void EncryptDES(bool key[56], bool outBlk[64], bool inBlk[64], int verbose);
-void DecryptDES(bool key[56], bool outBlk[64], bool inBlk[64], int verbose);
-
-#endif
