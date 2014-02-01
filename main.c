199832db99fadf7ce9628fc46ba218b96590c2f9
amper43 <amper43@ya.ru>
2014-02-01 13:34:23 +0400

    final

--- main.c
+++ main.c
@@ -2,7 +2,41 @@
 #include <string.h>
 
 #include "RS232.h"
+#include "DES.h"
  
+void bitTransToMas(char inBytes[8],char outMas[64])
+{
+
+	long long int byte64 = 0;
+	long long int mask;
+	int i;
+	memcpy(&byte64, inBytes, 8);
+
+	for(i = 0; i < 64; i++) 
+	{
+		mask = 1i64 << (63 - i);
+		if (byte64 & mask) outMas[i] = 1;
+		else outMas[i] = 0;
+
+	}
+
+}
+
+
+void masTransToBit(char outBytes[8],char inMas[64])
+{
+
+	long long int byte64 = 0;
+	int i = 0;
+	
+
+	for(i = 0; i < 64; i++) if(inMas[i]) byte64 |= 1i64 << (63 - i);
+
+	memcpy(outBytes, &byte64, 8);
+
+}
+
+
 
 int main(int argc, char *argv[] )
 {
@@ -26,8 +60,11 @@ int main(int argc, char *argv[] )
 	char key[56] = {0};
 	char outBlk[64] = {0};
 	char inBlk[64] = {0};
-	int verbose = 1;
+	char mode = 0;
+	long long int byteTag = 0;
 
+
+	int kk[4] = {0};
 	
 	if(argc < 2)
 	{
@@ -75,13 +112,48 @@ int main(int argc, char *argv[] )
 	printf("%d bytes total\n", i);
 	totalBytes = i;
 	*_totalBytes = i;
-	//fwrite(_totalBytes,sizeof(long long int),1,outfile);
 	infile = fopen(argv[1],"rb");
 
+	/*
+	EncryptDES(key, outBlk, inBlk, verbose);
 
-	EncryptDES(key, outBlk[64], inBlk[64], verbose);
+	printf("\n\n\noutBlk=\n\n\n");
+	for (i = 0; i < 64; i++)
+	printf("%d",outBlk[i]);
+	DecryptDES(key, inBlk, outBlk, verbose);
+	printf("\noutBlk\n");
+	for (i = 0; i < 64; i++)
+	printf("%d",inBlk[i]);
+*/
 
+	
+	/*
+	printf("\n\nbitTra=\n");
+	for (i = 0; i < 8; i++)
+		printf("%d",bytTra[i]);
+	printf("\n\n\noutBlkTrans=\n\n\n");
+	for (i = 0; i < 64; i++)
+	printf("%d",outBlk[i]);
 
+	bitTransToMas(bytTra,outBlk);
+	
+	printf("\n\n\noutBlkTrans=\n\n\n");
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0) printf("|");
+	printf("%d",outBlk[i]);
+
+	}
+	masTransToBit(bytTra,outBlk);
+printf("\n\nbitTra=\n");
+	
+	for (i = 0; i < 8; i++)
+	
+		printf("%d",bytTra[i]);
+	return 0;
+
+	*/
+	
 	printf("Ports:\n");
 	while(!(comCount = EnumerateSerialPorts(comName, 10)));
 	Sleep(100);
@@ -104,11 +176,22 @@ int main(int argc, char *argv[] )
 		}	
 
 
-	printf("start encryption\n");
+	printf("encrypt or decrypt? [e/d]");
+	scanf("%c",&mode);
+
+	writePort(&hSerial, &mode,1);
+	readPort(&hSerial, dst,30,timeout);
+	printf(dst);
+	printf("\n");
+
+	if (mode == 'e') fwrite(_totalBytes,sizeof(long long int),1,outfile);
+	else fread(&byteTag,sizeof(long long int),1,infile);
+
+	printf("start %s\n", (mode == 'e'? "encrypt" : "decrypt"));
 	while(totalBytes > 0)
 	{
 
-		if (totalBytes < 8) 
+		if (totalBytes < 8 && mode == 'e') 
 		{
 			fread(readBuf,sizeof(char),totalBytes,infile);
 			for(i = totalBytes; i < 8; i++) readBuf[i] = '0';
@@ -131,14 +214,42 @@ int main(int argc, char *argv[] )
 			printf("read error\n");
 			return 1;
 		}
-		else printf(dst);
 		printf("%d / ",*_totalBytes - totalBytes);
 		printf("%d\n",*_totalBytes);
-		fwrite(dst,sizeof(char),8,outfile);
-	}
-	printf("end encryption\n");
 
-	if (closePort(&hSerial)) printf("close error\n");
+
+		if ((mode == 'd') && (totalBytes == 8)) 
+		{
+			fwrite(dst,sizeof(char),byteTag % 8,outfile); 
+			printf("%llu / %llu\n", *_totalBytes, *_totalBytes);
+			break;
+		}
+		else fwrite(dst,sizeof(char),8,outfile);
+	}
+	printf("end %s\n", (mode == 'e'? "encrypt" : "decrypt"));
+
+	if (closePort(&hSerial)) printf("port close error\n");
+
+
+	if (tempFile)
+	{
+	if (fclose(outfile)) printf("error close file");
+	if (fclose(infile)) printf("error close file");
+	outfile = fopen(argv[1],"wb");
+	infile = fopen(fname,"rb");
+	mode = getc(infile);
+	while (!feof(infile))
+	{
+		 //mode is a simple buffer
+		fwrite(&mode,sizeof(char),1,outfile);
+		mode = getc(infile);
+		
+	} 
+
+	}
+
+	fclose(outfile);
+	fclose(infile);
 	if (tempFile) 
 	{
 		DeleteFile(fname);
